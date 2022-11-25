d11b5568108fdd60e769abd9fa8562fa42e42222
Roger Mullins <roger.mullins@student.awesomeinc.org>
2022-11-25 18:34:21 +0000

    Added branding and buttons

--- ereader/src/Header.js
+++ ereader/src/Header.js
@@ -2,13 +2,22 @@ import { Book } from 'react-bootstrap-icons';
 
 function Header() {
     return (
-        <nav class="navbar bg-light">
-           <div class="container-fluid">
-                <a class="navbar-brand" href="#">
-                    <Book alt="Logo" width="30" height="24" className="d-inline-block align-text-top"/> Roger's eReader
-                </a>
+        <div className="container">
+            <div className="row">
+                <div className="col">
+                    <a className="navbar-brand" href="#">
+                        <Book alt="Logo" width="30" height="24" className="d-inline-block align-text-top"/>
+                        Rog-eReader
+                    </a>
+                </div>
             </div>
-        </nav>
+            <nav className="row">
+                <a className="col-6 col-md-3 nav-item nav-link bg-danger p-1" href="#">The Public Domain</a>
+                <a className="col-6 col-md-3 nav-item nav-link bg-success p-1" href="#">About</a>
+                <a className="col-6 col-md-3 nav-item nav-link bg-warning p-1" href="#">Contact</a>
+                <a className="col-6 col-md-3 nav-item nav-link bg-info p-1" href="#">Log In / Register</a>
+            </nav>
+        </div>
     )
 };
 
