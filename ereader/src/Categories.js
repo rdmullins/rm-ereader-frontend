7a19348847e778c1c142c7a3268a5013b7289dd6
Roger Mullins <roger.mullins@student.awesomeinc.org>
2022-11-26 03:53:11 +0000

    Category buttons added

---
+++ ereader/src/Categories.js
@@ -0,0 +1,19 @@
+function Categories() {
+    return (
+        <div className="container">
+            <div className="row">
+                <div className="col-6 col-md-3 border">Category</div>
+                <div className="col-6 col-md-3 border">Category</div>
+                <div className="col-6 col-md-3 border">Category</div>
+                <div className="col-6 col-md-3 border">Category</div>
+                <div className="col-6 col-md-3 border">Category</div>
+                <div className="col-6 col-md-3 border">Category</div>
+                <div className="col-6 col-md-3 border">Category</div>
+                <div className="col-6 col-md-3 border">Category</div>
+            </div>
+        </div>
+    )
+
+};
+
+export default Categories
