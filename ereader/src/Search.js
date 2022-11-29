03ad5499addea86202c19b4e2d911b6b43878aff
Roger Mullins <roger.mullins@student.awesomeinc.org>
2022-11-29 03:37:10 +0000

    Bootstrap adjustments

--- ereader/src/Search.js
+++ ereader/src/Search.js
@@ -8,19 +8,19 @@ function Search(props) {
     // }
 
     function handleFormSubmit(e) {
-        console.log("Form submitted.");
-        console.log("Search Term: ", props.searchTerm);
-        console.log("Type of Search:");
+        // console.log("Form submitted.");
+        // console.log("Search Term: ", props.searchTerm);
+        // console.log("Type of Search:");
         if (e.target[2].checked) {
-            console.log("Title");
+            //console.log("Title");
             props.setSearchType("title");
         }
         if (e.target[3].checked) {
-            console.log("Author");
+            //console.log("Author");
             props.setSearchType("author");
         }
         if (e.target[4].checked) {
-            console.log("Subject");
+            //console.log("Subject");
             props.setSearchType("subject");
         }
         //props.setSearchTerm("");
@@ -33,31 +33,69 @@ function Search(props) {
             <div className="row">
                 <div className="col-1"></div>
                 <div className="col-10 input-group mb-3">
-                    <input type="text" onInput={(event) => props.setSearchTerm(event.target.value)} value={props.searchTerm} className="form-control" placeholder="Search..." aria-label="Search Box" aria-describedby="searchButton"></input>
+                    <input 
+                        type="text" 
+                        onInput={(event) => 
+                            props.setSearchTerm(event.target.value)} 
+                        value={props.searchTerm} 
+                        className="form-control" 
+                        placeholder="Search..." 
+                        aria-label="Search Box" 
+                        aria-describedby="searchButton">
+                    </input>
                     <button 
                         className="btn btn-outline-secondary" 
                         type="submit" 
-                        id="searchButton" 
-                        onClick={() => {
-                            //handleClick(searchTerm); 
-                            //setSearchTerm('');
-                        }}>Search</button>
+                        id="searchButton">
+                            Search
+                    </button>
                 </div>
                 <div className="col-1"></div>
             </div>
             <div className="row text-center">
                 <ul className="list-group list-group-horizontal">
                     <li className="col-4 list-group-item border-0">
-                        <input className="form-check-input me-1" type="radio" name="searchGroup" value="" id="title" defaultChecked={true}></input>
-                        <label className="form-check-label" htmlFor="title">Title</label>
+                        <input 
+                            className="form-check-input me-1" 
+                            type="radio" 
+                            name="searchGroup" 
+                            value="" 
+                            id="title" 
+                            defaultChecked={true}>
+                        </input>
+                        <label 
+                            className="form-check-label" 
+                            htmlFor="title">
+                                Title
+                        </label>
                     </li>
                     <li className="col-4 list-group-item border-0">
-                        <input className="form-check-input me-1" type="radio" name="searchGroup" value="" id="author"></input>
-                        <label className="form-check-label" htmlFor="author">Author</label>
+                        <input 
+                            className="form-check-input me-1" 
+                            type="radio" 
+                            name="searchGroup" 
+                            value="" 
+                            id="author">
+                        </input>
+                        <label 
+                            className="form-check-label" 
+                            htmlFor="author">
+                                Author
+                        </label>
                     </li>
                     <li className="col-4 list-group-item border-0">
-                        <input className="form-check-input me-1" type="radio" name="searchGroup" value="" id="subject"></input>
-                        <label className="form-check-label" htmlFor="subject">Subject</label>
+                        <input 
+                            className="form-check-input me-1" 
+                            type="radio" 
+                            name="searchGroup" 
+                            value="" 
+                            id="subject">
+                        </input>
+                        <label 
+                            className="form-check-label" 
+                            htmlFor="subject">
+                                Subject
+                        </label>
                     </li>
                 </ul>
             </div>
